de93779122eba27836d0c748da3c21e29b231afc
starkzarn <starkzarn@zarn.me>
2024-01-01 17:25:18 -0800

    best practices

--- scantopl.go
+++ scantopl.go
@@ -16,8 +16,9 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var (
-	Version = "v1.0.0"
+const (
+	Version       = "v1.0.1"
+	PauseDuration = 1 * time.Second
 )
 
 func FilenameWithoutExtension(fn string) string {
@@ -32,6 +33,7 @@ func createForm(form map[string]string) (string, io.Reader, error) {
 	body := new(bytes.Buffer)
 	mp := multipart.NewWriter(body)
 	defer mp.Close()
+
 	for key, val := range form {
 		if strings.HasPrefix(val, "@") {
 			val = val[1:]
@@ -40,6 +42,7 @@ func createForm(form map[string]string) (string, io.Reader, error) {
 				return "", nil, err
 			}
 			defer file.Close()
+
 			part, err := mp.CreateFormFile(key, val)
 			if err != nil {
 				return "", nil, err
@@ -56,34 +59,69 @@ func uploadFile(document, plurl, pltoken string) {
 	form := map[string]string{"document": "@" + document, "title": TitleFromFileName(document)}
 	ct, body, err := createForm(form)
 	if err != nil {
-		log.Fatal(err)
+		log.Error("Error creating form:", err)
+		return
 	}
+
 	req, err := http.NewRequest("POST", plurl+"/api/documents/post_document/", body)
+	if err != nil {
+		log.Error("Error creating HTTP request:", err)
+		return
+	}
+
 	req.Header.Set("Content-Type", ct)
 	req.Header.Set("Authorization", "Token "+pltoken)
+
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		log.Error("Error uploading file:", err)
+		return
 	}
+
 	if resp.StatusCode != http.StatusOK {
-		log.Warn("Upload failed with response code: ", resp.StatusCode)
+		log.Warn("Upload failed with response code:", resp.StatusCode)
 	} else {
-		log.Info("Upload succesfully, remove file")
+		log.Info("Upload successful, removing file:", document)
 		err := os.Remove(document)
 		if err != nil {
-			log.Warn(err)
+			log.Warn("Error removing file:", err)
 		}
 	}
 }
 
-func main() {
+func watchDirectory(watcher *fsnotify.Watcher, done chan bool, plurl, pltoken string) {
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				done <- true
+				return
+			}
+			handleFileEvent(event, plurl, pltoken)
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				done <- true
+				return
+			}
+			log.Warn("Error watching directory:", err)
+		}
+	}
+}
 
-	// OPTs
+func handleFileEvent(event fsnotify.Event, plurl, pltoken string) {
+	if event.Has(fsnotify.Create) && strings.HasPrefix(path.Base(event.Name), "pl_") {
+		time.Sleep(PauseDuration)
+		log.Info("New file to upload:", event.Name)
+		uploadFile(event.Name, plurl, pltoken)
+	}
+}
+
+func main() {
 	flag.String(flag.DefaultConfigFlagname, "", "path to config file")
-	scandir := flag.String("scandir", "/home/scanservjs/output", "Scanserjs ouput directory")
+	scandir := flag.String("scandir", "/home/scanservjs/output", "Scanserjs output directory")
 	plurl := flag.String("plurl", "http://localhost:8080", "The paperless instance URL without trailing /")
-	pltoken := flag.String("pltoken", "xxxxxxxxxxxxxxxxxx", "Paperless auth token , generated through admin")
+	pltoken := flag.String("pltoken", "xxxxxxxxxxxxxxxxxx", "Paperless auth token, generated through admin")
 	showversion := flag.Bool("version", false, "Show version and exit")
 
 	flag.Parse()
@@ -93,45 +131,21 @@ func main() {
 		os.Exit(0)
 	}
 
-	// test if it's a directory
 	log.Println("Start watching:", *scandir)
 
-	// The watch loop
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error creating watcher:", err)
 	}
 	defer watcher.Close()
 
 	done := make(chan bool)
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				//log.Println("event:", event)
-				if event.Has(fsnotify.Create) && strings.HasPrefix(path.Base(event.Name), "pl_") {
-					//little pause to ensure the write operation is finished
-					time.Sleep(1 * time.Second)
-					log.Info("New file to upload:", event.Name)
-					uploadFile(event.Name, *plurl, *pltoken)
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Warn("error:", err)
-			}
-		}
-	}()
+	go watchDirectory(watcher, done, *plurl, *pltoken)
 
-	//err = watcher.Add("/home/docker_data/scanservjs/output")
 	err = watcher.Add(*scandir)
-
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error adding directory to watcher:", err)
 	}
 	<-done
 }
+
